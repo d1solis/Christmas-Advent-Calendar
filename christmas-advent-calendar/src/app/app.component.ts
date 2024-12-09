7e026d6fc65a2b93fe444963fee3a432d4003126
d1solis <d1solis@ucsd.edu>
2024-12-08 18:54:34 -0800

    Fixed Routing

--- christmas-advent-calendar/src/app/app.component.ts
+++ christmas-advent-calendar/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CalendarComponent } from "./calendar/calendar.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { SnowComponent } from './snow/snow.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalendarComponent, SnowComponent],
+  imports: [RouterOutlet, SnowComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'christmas-advent-calendar';
