21d38b7f5ce301ff26786a4bdca946f81a3c00a3
d1solis <d1solis@ucsd.edu>
2024-12-08 16:07:51 -0800

    Generated snow and fixed bugs

--- christmas-advent-calendar/src/app/app.component.ts
+++ christmas-advent-calendar/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from "./calendar/calendar.component";
+import { SnowComponent } from './snow/snow.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalendarComponent],
+  imports: [RouterOutlet, CalendarComponent, SnowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ christmas-advent-calendar/src/app/snow/snow.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snow',
+  imports: [],
+  templateUrl: './snow.component.html',
+  styleUrl: './snow.component.css'
+})
+export class SnowComponent {
+
+}
