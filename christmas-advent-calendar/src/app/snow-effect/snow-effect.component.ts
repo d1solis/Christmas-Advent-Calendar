312da2429a0577c2625bb215b170f1eb301f4bdc
d1solis <d1solis@ucsd.edu>
2024-12-11 14:44:12 -0800

    Generated snow effect component

---
+++ christmas-advent-calendar/src/app/snow-effect/snow-effect.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snow-effect',
+  imports: [],
+  templateUrl: './snow-effect.component.html',
+  styleUrl: './snow-effect.component.css'
+})
+export class SnowEffectComponent {
+
+}
