6b411e06ff47f94fd8a63ee3c4adf971792377d0
d1solis <d1solis@ucsd.edu>
2024-12-11 00:20:16 -0800

    Removed snow component

--- christmas-advent-calendar/src/app/app.component.ts
+++ christmas-advent-calendar/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { SnowComponent } from './snow/snow.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SnowComponent, RouterModule],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
