25655394f50498045e930d63917dcbe15059f80b
d1solis <d1solis@ucsd.edu>
2024-12-11 22:41:03 -0800

    Created page-3

---
+++ christmas-advent-calendar/src/app/page-3/page-3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-3',
+  imports: [],
+  templateUrl: './page-3.component.html',
+  styleUrl: './page-3.component.css'
+})
+export class Page3Component {
+
+}
