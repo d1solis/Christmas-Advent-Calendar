4f65679e6b148975146a50000ef54498a169cbad
d1solis <d1solis@ucsd.edu>
2024-12-21 12:11:02 -0800

    Page-21 complete

--- christmas-advent-calendar/src/app/page-21/page-21.component.ts
+++ christmas-advent-calendar/src/app/page-21/page-21.component.ts
@@ -9,5 +9,14 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./page-21.component.css']
 })
 export class Page21Component {
+  gifts = [
+    { visible: false }, // Gift 1 visibility state
+    { visible: false }, // Gift 2 visibility state
+    { visible: false }, // Gift 3 visibility state
+  ];
 
+  // Toggle visibility for the selected gift
+  toggleVisibility(index: number) {
+    this.gifts[index].visible = !this.gifts[index].visible;
+  }
 }
