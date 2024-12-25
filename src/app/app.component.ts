008700110b0cd497619b12d1180c2e058ad6d5a0
d1solis <d1solis@ucsd.edu>
2024-12-24 23:55:26 -0800

    "redirect" changes for main web page deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,19 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { SnowEffectComponent } from "./snow-effect/snow-effect.component";
 import { Router, NavigationEnd } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterModule, SnowEffectComponent],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-/*
-export class AppComponent {
-  title = 'christmas-advent-calendar';
-}
-  */
+
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
-
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
